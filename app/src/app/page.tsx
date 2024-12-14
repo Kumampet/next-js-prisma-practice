273bd160e9e5a26a8fdedcba3c9d9a22f1493c16
Kumampet <kumampet@gmail.com>
2024-12-14 13:02:09 +0900

    fix

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -5,6 +5,15 @@ import axios from 'axios';
 import _isEmpty from 'lodash/isEmpty';
 import _get from 'lodash/get';
 import { PostTypes } from './types';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper
+} from '@mui/material';
 
 const fetchData = async (): Promise<PostTypes[]> => {
   const _axios = axios.create({
@@ -19,6 +28,35 @@ const fetchData = async (): Promise<PostTypes[]> => {
     return [];
   }
   return res.data;
+}
+
+const DataTable = (props: { rows: PostTypes[] }) => {
+  const { rows } = props;
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">ID</TableCell>
+            <TableCell align="right">Title</TableCell>
+            <TableCell align="right">Content</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">{row.id}</TableCell>
+              <TableCell align="right">{row.title}</TableCell>
+              <TableCell align="right">{row.content}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
 export default function Home() {
@@ -36,24 +74,7 @@ export default function Home() {
     <div>
       <h1>Post</h1>
       <p>Get started by editing <code>pages/index.js</code>.</p>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Content</th>
-          </tr>
-        </thead>
-        <tbody>
-          {posts.map((post) => (
-            <tr key={post.id}>
-              <td>{post.id}</td>
-              <td>{post.title}</td>
-              <td>{post.content}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <DataTable rows={posts} />
     </div>
   );
 }
